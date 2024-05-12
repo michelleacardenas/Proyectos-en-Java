cdabd5f9de173e93335502a8f2088512951ea9b2
Michelle Alejandra Cárdenas Reyes <mikopqr74@gmail.com>
2024-05-11 23:20:55 -0600

    Add files via upload

---
+++ POO/Excepciones/src/poo/PrbLecturas.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package poo;
+
+/**
+ *
+ * @author marip
+ */
+public class PrbLecturas
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        // TODO code application logic here
+        System.out.println("Dame un entero: ");
+        //int x= Lecturas.leerEntero();
+         int x=Lecturas.leerEntero(true);
+        System.out.println(x);
+        
+        System.out.println("Dame un double: ");
+        double y= Lecturas.leerDouble(true);
+        System.out.println(y);
+        
+        System.out.println("Dame un carácter: ");
+        char z= Lecturas.leerChar();
+        System.out.println(z);
+        
+        System.out.println("Dame una cadena: ");
+        String v= Lecturas.leerString();
+        System.out.println(v);
+        
+        
+    }
+    
+}
